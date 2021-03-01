e965dab439e70d6c08d53a4eb9938056c1be32f2
m rachmat riadi <m.rachmat.riadi@gmail.com>
2021-03-01 17:09:30 +0700

    update

--- challenge/1-pattern1.php
+++ challenge/1-pattern1.php
@@ -7,7 +7,6 @@ for ($i=0; $i < 10; $i++) {
         }else{
             echo ".";
         }
-        
     }
     echo "<br>";
 }
--- challenge/28-papan_catur.php
+++ challenge/28-papan_catur.php
@@ -6,22 +6,17 @@
     <title>Papan catur</title>
 </head>
 <body>
-<?php 
-function blackWhite($val){
-    if($val % 2 == 0 ){
-        return "black";
-    }else{
-        return "white";
-    }
-}
-?>
     <table border="1px" >
         <?php 
         $c = 0;
         for ($row=0; $row < 8; $row++) { 
             echo '<tr>';
             for ($col=0; $col < 8; $col++) { 
-                $color = blackWhite($c);
+                if($c % 2 == 0 ){
+                    $color = "black";
+                }else{
+                    $color = "white";
+                }
                 echo '<td style="width:50px;height:50px;background-color:'.$color.'"></td>';
                 $c++;
             }
